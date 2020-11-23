d2aae9ece0125f8a0ca26403894c7f435bd8bc14
Jannik <6362150+janniks@users.noreply.github.com>
2020-11-23 18:27:08 +0100

    bump version

--- Formula/git-ignore.rb
+++ Formula/git-ignore.rb
@@ -2,9 +2,9 @@
 class GitIgnore < Formula
   desc "An interactive git command to generate .gitignore files (written in Rust)"
   homepage "https://github.com/janniks/git-ignore"
-  url "https://github.com/janniks/git-ignore/releases/download/0.1.1/git-ignore-mac.tar.gz"
-  sha256 "afcce1913779ea317d21e103784b43aeb786f81fe0392fcfb398088d70f8f06c"
-  version "0.1.1"
+  url "https://github.com/janniks/git-ignore/releases/download/0.2.0/git-ignore-mac.tar.gz"
+  sha256 "d2c467296778e2b93849cf0502a1292091e372281901dc2da481be1f7b7b1abf"
+  version "0.2.0"
 
   def install
     bin.install "git-ignore"
