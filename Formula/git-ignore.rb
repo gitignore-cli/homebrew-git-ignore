18861c814e8a6032b7e56f752e6d06ae97264f0f
makp0 <maxsu4@gmail.com>
2025-08-10 01:56:56 +0200

    Update git-ignore.rb

--- Formula/git-ignore.rb
+++ Formula/git-ignore.rb
@@ -1,8 +1,8 @@
 # https://docs.brew.sh/Formula-Cookbook
 class GitIgnore < Formula
   desc "An interactive git command to generate .gitignore files (written in Rust)"
-  homepage "https://github.com/makp0/git-ignore"
-  url "https://github.com/makp0/git-ignore/releases/download/v0.2.1/git-ignore-x86_64-apple-darwin.tar.gz"
+  homepage "https://github.com/gitignore-cli/git-ignore"
+  url "https://github.com/gitignore-cli/git-ignore/releases/download/v0.2.1/git-ignore-x86_64-apple-darwin.tar.gz"
   sha256 "b5e3474bc98557a829367e1be4c79403fff39b66e15dfe0c1d6904917fd26a45"
   version "0.2.1"
 
